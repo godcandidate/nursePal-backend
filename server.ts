23513a8b49296bcdfcbf71ac1d4ece8d82b75d77
godcandidate <godcandidate101@gmail.com>
2025-04-07 15:37:06 +0000

    feat: add mongodb config

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
 import "dotenv/config";
+import connectDB from "./utils/db";
 
 //create server
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is connected http://localhost:${process.env.PORT}`);
 });
