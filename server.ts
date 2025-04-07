26242afbc99fcf893f8be7e683c2972176f288a7
godcandidate <godcandidate101@gmail.com>
2025-04-07 12:57:24 +0000

    chore: init project

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import "dotenv/config";
+
+//create server
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is connected http://localhost:${process.env.PORT}`);
+});
