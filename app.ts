963c58b7c7599e111e37cb41840928637cfab618
godcandidate <godcandidate101@gmail.com>
2025-04-17 09:36:18 +0000

    Update CORS configuration to enable production origin

--- app.ts
+++ app.ts
@@ -18,8 +18,8 @@ app.use(cookieParser());
 //cors
 app.use(
   cors({
-    //origin: "https://nursepal.onrender.com",
-    origin: "http://localhost:5173",
+    origin: "https://nursepal.onrender.com",
+    // origin: "http://localhost:5173",
     credentials: true, 
   })
 );
